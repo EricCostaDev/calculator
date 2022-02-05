ac9f1a7aa4846c82c8eaa4d1d4381af0d1481ae8
Eric <ecosta@bairru.com.br>
2022-02-05 20:47:55 -0300

    fixed - color of the button text is white when bg is black and black when bg is not black

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
 
 export const ButtonText = styled.Text`
-  color: ${(props: { color: string; }) => props.color === '#222' ? '#fff' : '#fff'};
-  font-size: 24px;
+  color: ${(props: { color: string; }) => props.color === '#222' ? '#fff' : '#222'};
+  font-size: ${(props: { type: string; }) => {if (props.type === 'small'){return '18px'} else {return '24px'}}};
   font-weight: 600;
 `;
 
@@ -15,4 +15,15 @@ export const ButtonContainer = styled.TouchableOpacity`
   border-radius: 64px;
   margin-right: 5px;
   margin-bottom: 10px;
-`
+`;
+export const SmallButtonContainer = styled.TouchableOpacity`
+width: 50px;
+height: 38px;
+justify-content: center;
+align-items: center;
+background-color: transparent;
+border-radius: 64px;
+margin-right: 5px;
+margin-bottom: 10px;
+border: 1px solid white;
+`;
