cbf65e68b493ee304da1e3ddf332027cbf5774d4
Eric <ecosta@bairru.com.br>
2022-02-05 20:45:48 -0300

    fixed - color of the button text is white when bg is black and black when bg is not black

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React, { ReactElement } from "react";
-import { ButtonContainer, ButtonText } from "./styles";
+import { ButtonContainer, ButtonText, SmallButtonContainer } from "./styles";
 
 export interface IButtonProps {
   type?: string;
@@ -15,10 +15,22 @@ const Button = ({
   onPress,
   color,
 }: IButtonProps): ReactElement => {
+  if (type === "small") {
+    return (
+      <>
+        <SmallButtonContainer color={"#222"} onPress={onPress}>
+          <ButtonText type="small" color={"#222"}>
+            {title}
+          </ButtonText>
+        </SmallButtonContainer>
+      </>
+    );
+  }
+
   return (
     <>
-      <ButtonContainer color={color || "#222"} onPress={onPress}>
-        <ButtonText>{title}</ButtonText>
+      <ButtonContainer color={color ? color : "#222"} onPress={onPress}>
+        <ButtonText color={color ? color : "#222"}>{title}</ButtonText>
       </ButtonContainer>
     </>
   );
